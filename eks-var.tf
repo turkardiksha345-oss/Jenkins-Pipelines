3d47de3b5a0614dec78cf368e0404de6cc20ed88
Diksha Turkar <turkardiksha345@gmail.com>
2026-02-08 00:32:08 +0530

    Create eks-var.tf

---
+++ eks-var.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "my-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.micro"
+}
+
+variable "desired_nodes" {
+  default = 2
+}
+
